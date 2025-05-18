ffbda54cf783992361439d8d2c4011381cccf40b
Anton787 <antongagelganc6443@gmail.com>
2025-05-19 04:24:59 +0500

    full-functional

---
+++ src/pages/feed/feed.tsx
@@ -0,0 +1,29 @@
+import { fetchFeeds } from '@slices/feedsSlice';
+import { useDispatch, useSelector } from '@store';
+import { Preloader } from '@ui';
+import { FeedUI } from '@ui-pages';
+import { TOrder } from '@utils-types';
+import { FC, useEffect } from 'react';
+
+export const Feed: FC = () => {
+  const dispatch = useDispatch();
+
+  const orders: TOrder[] = useSelector((state) => state.feeds.data.orders);
+
+  useEffect(() => {
+    dispatch(fetchFeeds());
+  }, [dispatch]);
+
+  if (!orders.length) {
+    return <Preloader />;
+  }
+
+  return (
+    <FeedUI
+      orders={orders}
+      handleGetFeeds={() => {
+        dispatch(fetchFeeds());
+      }}
+    />
+  );
+};
---
+++ src/services/store/store.ts
@@ -0,0 +1,35 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import ingredientsReducer from '../slices/ingredientsSlice';
+import ordersReducer from '../slices/ordersSlice';
+import builderSlice from '../slices/builderSlice';
+import feedsSlice from '../slices/feedsSlice';
+import userReducer from '../slices/userSlice';
+
+import {
+  TypedUseSelectorHook,
+  useDispatch as dispatchHook,
+  useSelector as selectorHook
+} from 'react-redux';
+import { ordersMiddleware } from '../slices/orders';
+
+const rootReducer = combineReducers({
+  ingredients: ingredientsReducer,
+  orders: ordersReducer,
+  builder: builderSlice,
+  feeds: feedsSlice,
+  user: userReducer
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(ordersMiddleware),
+  devTools: process.env.NODE_ENV !== 'production'
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export const useDispatch: () => AppDispatch = () => dispatchHook();
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
